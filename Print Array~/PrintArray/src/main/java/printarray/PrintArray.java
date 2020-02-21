6aa4e84aa379fbb56c1d7ee27c19003ba49de4fc
Dinesh Nanda <dannynanda76@gmail.com>
2020-02-21 17:28:21 -0500

    Print Array

---
+++ Print Array~/PrintArray/src/main/java/printarray/PrintArray.java
@@ -0,0 +1,18 @@
+package printarray;
+
+/**
+ *
+ * @author Dinesh Nanda
+ */
+
+public class PrintArray {
+
+    public static void main(String[] args) {
+        
+        int arr [] = {3, 1, 5, 2, 9};
+        
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
